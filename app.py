2f8eb937bffbd0072913f787e749affaca478d93
pds0309 <76927397+pds0309@users.noreply.github.com>
2023-06-21 18:56:41 +0900

    feat: add morpheme api

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from konlpy.tag import Okt
 from nltk import word_tokenize, pos_tag
-from fuzzywuzzy import fuzz
 from googletrans import Translator
 import nltk
 import re
@@ -13,7 +12,9 @@ from db_connect import get_db
 
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
+nltk.download('stopwords')
 
+stopwords = nltk.corpus.stopwords.words('english')
 app = Flask(__name__)
 CORS(app)
 
@@ -21,7 +22,6 @@ CORS(app)
 # os.environ['JAVA_HOME'] = 'C:/Program Files/Java/jdk-17.0.4'
 okt = Okt()
 translator = Translator()
-
 
 @app.route('/')
 def home():
@@ -34,7 +34,8 @@ def post_nouns():
         data = request.get_json()
         sentence = data['sentence']
         user_id = data['userId']
-        eng_list = extract_english_keyword(sentence)
+        eng_dict = extract_english_keyword(sentence)
+        eng_list = [d['morph'] for d in eng_dict]
         kor_list = extract_korean_keyword(sentence)
         combined_list = eng_list + kor_list
         filtered_list = [word for word in combined_list if len(word) > 1]
@@ -57,33 +58,15 @@ def get_nouns(user_id):
         return jsonify({'message': 'bad request'}), 400
 
 
-@app.route('/api/scores/<string:user_id>', methods=['GET'])
-def get_scores(user_id):
-    try:
-        data = get_db().scores.find_one({'user_id': user_id}, {'_id': False})
-        total_score = 0
-        count = 0
-        if data is not None:
-            total_score = data['total_score']
-            count = data['count']
-        return jsonify({'totalScore': total_score, 'count': count, 'user': user_id})
-    except:
-        return jsonify({'message': 'bad request'}), 400
-
-
-@app.route('/api/scores', methods=['POST'])
-def post_scores():
+@app.route('/api/morpheme', methods=['PUT'])
+def put_morpheme():
     try:
         data = request.get_json()
-        answer = (data['answer']).upper()
-        input_value = (data['input']).upper()
-        user_id = data['userId']
-        score = max(get_string_distance(input_value, answer),
-                    get_string_distance(get_translated_text(input_value), get_translated_text(answer)))
-        weighted_score = get_weighted_score(score)
-        get_db().scores.update_one({'user_id': user_id}, {'$inc': {'count': 1, 'total_score': weighted_score}},
-                                   upsert=True)
-        return jsonify({'score': weighted_score})
+        user_answer = data['userAnswer']
+        card_answer = data['cardAnswer']
+        user_answer_morpheme = extract_english_keyword(get_translated_text(user_answer))
+        card_answer_morpheme = extract_english_keyword(get_translated_text(card_answer))
+        return jsonify({"userAnswerMorpheme": user_answer_morpheme, "cardAnswerMorpheme": card_answer_morpheme})
     except:
         return jsonify({'message': 'bad request'}), 400
 
@@ -91,8 +74,12 @@ def post_scores():
 def extract_english_keyword(keyword):
     english_text = re.sub('[^a-zA-Z]', ' ', keyword)
     english_tagged = pos_tag(word_tokenize(english_text))
-    english_noun_list = [word for word, pos in english_tagged if pos in ['NN', 'NNS', 'NNP', 'NNPS']]
-    return english_noun_list
+    english_list = []
+    for word, pos in english_tagged:
+        if word.lower() not in stopwords:
+            extracted = {'morph': word, 'pos': pos}
+            english_list.append(extracted)
+    return english_list
 
 
 def extract_korean_keyword(keyword):
@@ -100,21 +87,9 @@ def extract_korean_keyword(keyword):
     return okt.nouns(okt.normalize(korean_text))
 
 
-def get_string_distance(input_string, answer):
-    if len(input_string) == 1 and len(answer) == 1:
-        return fuzz.ratio(input_string, answer)
-    return fuzz.token_sort_ratio(input_string, answer)
-
-
 def get_translated_text(text):
     result = translator.translate(text, dest='en')
     return result.text.upper()
-
-
-def get_weighted_score(score):
-    if score == 100:
-        return score
-    return round(score ** 0.965)
 
 
 if __name__ == '__main__':
